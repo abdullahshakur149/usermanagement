3f8d081920c0c7fa76f9bb43053d192a39aebbaf
Abdullah Shakoor <abdullahshakoor@192.168.1.3>
2023-11-10 23:11:02 +0500

    fixed routes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,48 @@
+// Construct the base URL dynamically based on the deployment environment
+const apiUrl = process.env.VERCEL_ENV === 'production'
+  ? `https://${process.env.VERCEL_URL}/api/users`
+  : 'http://localhost:3000/api/users';
 
+$("#add_user").submit(function(event) {
+  alert("Data Inserted Successfully!");
+});
 
+$("#update_user").submit(function(event) {
+  event.preventDefault();
 
-$("#add_user").submit(function(event){
-    alert("Data Inserted Successfully!");
-})
+  var unindexed_array = $(this).serializeArray();
+  var data = {};
 
-$("#update_user").submit(function(event){
-    event.preventDefault();
+  $.map(unindexed_array, function(n, i) {
+    data[n['name']] = n['value'];
+  });
 
-    var unindexed_array = $(this).serializeArray();
-    var data = {}
+  var request = {
+    "url": `${apiUrl}/${data.id}`,
+    "method": "PUT",
+    "data": data
+  };
 
-    $.map(unindexed_array, function(n, i){
-        data[n['name']] = n['value']
-    })
+  $.ajax(request).done(function(response) {
+    alert("Data Updated Successfully!");
+  });
+});
 
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function() {
+    var id = $(this).attr("data-id");
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
-        "method" : "PUT",
-        "data" : data
+      "url": `${apiUrl}/${id}`,
+      "method": "DELETE"
+    };
+
+    if (confirm("Do you really want to delete this record?")) {
+      $.ajax(request).done(function(response) {
+        alert("Data Deleted Successfully!");
+        location.reload();
+      });
     }
-
-    $.ajax(request).done(function(response){
-        alert("Data Updated Successfully!");
-    })
-
-})
-
-if(window.location.pathname == "/"){
-    $ondelete = $(".table tbody td a.delete");
-    $ondelete.click(function(){
-        var id = $(this).attr("data-id")
-
-        var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
-            "method" : "DELETE"
-        }
-
-        if(confirm("Do you really want to delete this record?")){
-            $.ajax(request).done(function(response){
-                alert("Data Deleted Successfully!");
-                location.reload();
-            })
-        }
-
-    })
+  });
 }
